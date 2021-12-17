a104933ae7371dafd20d628786cf6d387734605b
SanketAmate3 <96285973+SanketAmate3@users.noreply.github.com>
2021-12-17 19:57:18 +0530

    Update GitDemo.java

--- GitHub/src/com/git/GitDemo.java
+++ GitHub/src/com/git/GitDemo.java
@@ -4,7 +4,11 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("OLA git :)");
-
+		m();
+	}
+	static public void m()
+	{
+		System.out.println("Pull from Github");
 	}
 
 }
