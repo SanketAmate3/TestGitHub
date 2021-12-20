e000296f12f73c23b14ad416d55ee59ab770d2e9
Aakash2500 <96283100+Aakash2500@users.noreply.github.com>
2021-12-20 15:11:05 +0530

    Update GitDemo.java

--- GitHub/src/com/git/GitDemo.java
+++ GitHub/src/com/git/GitDemo.java
@@ -5,6 +5,9 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("OLA git :)");
 		m();
+		System.out.println("OLA git111111111.. :)");
+		
+		
 	}
 	static public void m()
 	{
